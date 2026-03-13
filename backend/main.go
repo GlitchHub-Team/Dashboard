9dcd14a0c9d1c7b737d6f10b1385d73fcfef18cf
elia-stellin-unipd <eliaernesto.stellin@studenti.unipd.it>
2026-03-13 10:22:20 +0000

    [Backend] Impostato main con Fx

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,84 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+
+	// "backend/internal/common"
+	"backend/internal/alert"
+	"backend/internal/api_key"
+	"backend/internal/auth"
+	"backend/internal/gateway"
+	"backend/internal/historical_data"
+	"backend/internal/real_time_data"
+	"backend/internal/sensor"
+	"backend/internal/tenant"
+	"backend/internal/user"
+	"context"
+	"fmt"
+)
+
+func init() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Println("error loading .env")
+	}
+}
+
+
+func NewGinEngine(
+	lc fx.Lifecycle,
+	log *zap.Logger,
+	gatewayController *gateway.GatewayController,
+) *gin.Engine {
+	router := gin.Default()
+
+	lc.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			log.Info("Starting HTTP server!")
+			
+			public := router.Group("/")
+
+			{
+				// TODO: questo è solo un test
+				public.POST("/gateway", gatewayController.CreateGateway)
+				public.DELETE("/gateway", gatewayController.DeleteGateway)
+			}
+
+			go router.Run()
+			return nil
+		},
+		OnStop: func(context.Context) error {
+			log.Info("Stopping HTTP server!")
+			return nil
+		},
+	})
+
+	return router
+}
+
+
 
 func main() {
-	
+
+	fx.New(
+		gateway.Module,
+		alert.Module,
+		api_key.Module,
+		auth.Module,
+		gateway.Module,
+		historical_data.Module,
+		real_time_data.Module,
+		sensor.Module,
+		tenant.Module,
+		user.Module,
+		fx.Provide(
+			NewGinEngine,
+			zap.NewExample,
+		),
+		fx.Invoke(func(*gin.Engine) {}),
+		
+	).Run()
 }
