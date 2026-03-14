171a75ff34b2a6f32b6a5e940aab6bdbb515c433
elia-stellin-unipd <eliaernesto.stellin@studenti.unipd.it>
2026-03-14 00:02:55 +0000

    [Backend] impostato repository auth

--- backend/internal/auth/repository.go
+++ backend/internal/auth/repository.go
@@ -1,1 +1,13 @@
 package auth
+
+type passwordTokenPostgreRepository struct {}
+
+func newPasswordTokenPostgreRepository() *passwordTokenPostgreRepository {
+	return &passwordTokenPostgreRepository{}
+}
+
+type confirmTokenPostgreRepository struct {}
+
+func newConfirmTokenPostgreRepository() *confirmTokenPostgreRepository {
+	return &confirmTokenPostgreRepository{}
+}
