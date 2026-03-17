1ae48485e5dd933355f92baada3895296dbb7340
elia-stellin-unipd <eliaernesto.stellin@studenti.unipd.it>
2026-03-17 16:06:56 +0000

    [Backend] Cleanup errors

--- backend/internal/user/errors.go
+++ backend/internal/user/errors.go
@@ -7,7 +7,6 @@ import (
 var (
 	ErrUserAlreadyExists = errors.New("user already present")
 	ErrUserNotFound      = errors.New("user not found")
-	// var errUserCreationFailed = errors.New("user creation failed")
 	ErrUnknownRole = errors.New("unknown role")
 	ErrCannotSendEmail = errors.New("cannot create user: cannot send confirmation email")
 )
