8ba09793589478a1d62330d0b3dba0df86a33ee4
elia-stellin-unipd <eliaernesto.stellin@studenti.unipd.it>
2026-03-23 02:52:16 +0000

    [Backend] Aggiornato email package per applicare "consumer owns the interface"

--- backend/internal/email/module.go
+++ backend/internal/email/module.go
@@ -1,15 +1,20 @@
 package email
 
 import (
+	"backend/internal/auth"
+	"backend/internal/shared/config"
+	"backend/internal/user"
+
 	"go.uber.org/fx"
 	"go.uber.org/zap"
-	"backend/internal/config"
 )
 
 func NewEmailAdapterFactory(
 	cfg *config.Config,
 	log *zap.Logger,
-) SendEmailPort {
+) (
+	SendEmailPort,
+) {
 	// NOTA: Si può avere solo un email sender alla vota, quindi è importante usare questo
 	// factory per crearlo
 	// Bisogna inserire tutte le dipendenze di tutti i possibili adapter
@@ -23,6 +28,10 @@ func NewEmailAdapterFactory(
 var Module = fx.Module(
 	"email",
 	fx.Provide(
-		NewEmailAdapterFactory,
+		fx.Annotate(
+			NewEmailAdapterFactory,
+			fx.As(new(user.SendConfirmAccountEmailPort)),
+			fx.As(new(auth.SendChangePasswordEmailPort)),
+		),
 	),
 )
