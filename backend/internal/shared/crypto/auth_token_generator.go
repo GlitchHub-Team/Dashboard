e66f5d6f56a7a9b66f9961571ed616b2c8438775
elia-stellin-unipd <eliaernesto.stellin@studenti.unipd.it>
2026-03-25 18:09:54 +0000

    [Backend] Setup dei TU per backend/internal/auth

--- backend/internal/shared/crypto/auth_token_generator.go
+++ backend/internal/shared/crypto/auth_token_generator.go
@@ -6,6 +6,8 @@ import (
 	"backend/internal/shared/identity"
 )
 
+//go:generate mockgen -destination=../../../tests/shared/crypto/mocks/auth_token.go -package=mocks . AuthTokenManager
+
 type AuthTokenManager interface {
 	GenerateForRequester(requester identity.Requester) (string, error)
 	GetRequesterFromToken(token string) (identity.Requester, error)
--- backend/internal/shared/crypto/security_token_generator.go
+++ backend/internal/shared/crypto/security_token_generator.go
@@ -4,6 +4,8 @@ import (
 	"time"
 )
 
+//go:generate mockgen -destination=../../../tests/shared/crypto/mocks/security_token.go -package=mocks . SecurityTokenGenerator
+
 type SecurityTokenGenerator interface {
 	/* Genera un token e la sua versione hashed */
 	GenerateToken() (encodedToken string, hashedToken string, err error)
