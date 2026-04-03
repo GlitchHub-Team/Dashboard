f852e96753c188046aaa7a3d18c64c67d1634e59
Riccardo-Graziani-04 <riccardograziani4@gmail.com>
2026-04-03 14:12:59 +0000

    [Frontend] Fix typos in alcuni adapters

--- frontend/frontend/src/app/adapters/sensor-historic/sensor-historic.adapter.ts
+++ frontend/frontend/src/app/adapters/sensor-historic/sensor-historic.adapter.ts
@@ -1,7 +1,8 @@
 import { HistoricReadings } from '../../models/sensor-data/historic-readings.model';
 import { SensorReading } from '../../models/sensor-data/sensor-reading.model';
+import { HistoricResponse } from '../../models/sensor-data/historic-response.model';
 
 export abstract class SensorHistoricAdapter {
   abstract fromDTO(value: number, timestamp: number): SensorReading;
-  abstract fromResponse(response: unknown): HistoricReadings;
+  abstract fromResponse(response: HistoricResponse): HistoricReadings;
 }
--- frontend/frontend/src/app/adapters/sensor-live/sensor-live-reading.adapter.ts
+++ frontend/frontend/src/app/adapters/sensor-live/sensor-live-reading.adapter.ts
@@ -1,5 +1,6 @@
 import { SensorReading } from '../../models/sensor-data/sensor-reading.model';
+import { RealTimeReading } from '../../models/sensor-data/real-time-reading.model';
 
 export abstract class SensorLiveReadingAdapter {
-  abstract fromDTO(dto: unknown): SensorReading;
+  abstract fromDTO(dto: RealTimeReading): SensorReading;
 }
