8e5de469d6d886e1790c9a8d724d92726cb7624f
NotHoss <hossnv@gmail.com>
2026-04-10 11:37:59 +0200

    Le metriche ora sono aggregate per route template

--- backend/internal/infra/metrics/module.go
+++ backend/internal/infra/metrics/module.go
@@ -13,6 +13,12 @@ var Module = fx.Module(
 
 func RegisterPrometheus(router *gin.Engine) {
 	p := ginprometheus.NewPrometheus("gin")
+	p.ReqCntURLLabelMappingFn = func(ctx *gin.Context) string {
+		if path := ctx.FullPath(); path != "" {
+			return path
+		}
+		return ctx.Request.URL.Path
+	}
 
 	p.Use(router)
 }
